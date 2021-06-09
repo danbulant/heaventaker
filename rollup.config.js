5361091fc2ac3e18a8a9874a1cd50ad9534abad8
Daniel Bulant <danbulant@gmail.com>
2021-06-09 17:13:12 +0200

    add --host

--- rollup.config.js
+++ rollup.config.js
@@ -19,7 +19,7 @@ function serve() {
 	return {
 		writeBundle() {
 			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev', '--host', '0.0.0.0'], {
 				stdio: ['ignore', 'inherit', 'inherit'],
 				shell: true
 			});
